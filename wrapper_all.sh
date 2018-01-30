15f0b900211a422adc98441a18415a81e3f3cbdb
Mads Jensen <mje.mads@gmail.com>
2018-01-30 11:32:22 +0100

    change in mem

--- wrapper_all.sh
+++ wrapper_all.sh
@@ -10,14 +10,7 @@ elif [ $# == 3 ]
   then
     for sub in $(<subjects.txt)
     do
-        eval submit_to_cluster -q $2 -m $3 -n 2 \"python $1 $sub\"
-    done
-
-elif [ $# == 4 ]
-  then
-    for sub in $(<subjects.txt)
-    do
-        eval submit_to_cluster -q $2 -m $3 -n $4 \"python $1 $sub\" 
+        eval submit_to_cluster -q $2 -n $3  \"python $1 $sub\"
     done
 
 else
