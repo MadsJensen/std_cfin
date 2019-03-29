ce65d9fa612ba8cc9de8b7a7bbc1f8c50ea1191e
Mads Jensen <mje.mads@gmail.com>
2019-03-29 11:19:26 +0100

    ENH: added n_jobs option

--- wrapper_all.sh
+++ wrapper_all.sh
@@ -13,6 +13,13 @@ elif [ $# == 3 ]
         eval submit_to_cluster -q $2 -n $3  \"python $1 $sub\"
     done
 
+elif [ $# == 4 ]
+  then
+    for sub in $(<subjects.txt)
+    do
+        eval submit_to_cluster -q $2 -n $3  \"python $1 $sub $5\"
+    done
+
 else
     for sub in $(<subjects.txt)
     do
