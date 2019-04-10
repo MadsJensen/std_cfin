8de6edfe9418f12497f9221436e45a11a3ca1fc4
Mads Jensen <mje.mads@gmail.com>
2019-04-10 13:18:02 +0200

    FIX: ?

--- wrapper.sh
+++ wrapper.sh
@@ -1,30 +1,28 @@
-subjects = $1
-
 if [ $# == 2 ]
   then
-    for sub in $(<subjects)
+    for sub in $(<$2)
     do
-        eval submit_to_cluster -q $3 -n 2 \"python $2 $sub\"
+        eval submit_to_cluster -q $3 -n 2 \"python $1 $sub\"
     done
 
 elif [ $# == 3 ]
   then
-    for sub in $(<subjects)
+    for sub in $(<$2)
     do
-        eval submit_to_cluster -q $3 -n $4  \"python $2 $sub\"
+        eval submit_to_cluster -q $3 -n $4  \"python $1 $sub\"
     done
 
 elif [ $# == 4 ]
   then
-    for sub in $(<subjects)
+    for sub in $(<$2)
     do
-        eval submit_to_cluster -q $3 -n $4  \"python $2 $sub $5\"
+        eval submit_to_cluster -q $3 -n $4  \"python $1 $sub $5\"
     done
 
 else
-    for sub in $(<subjects)
+    for sub in $(<$2)
     do
-        eval submit_to_cluster -n 2 \"python $2 $sub\"
+        eval submit_to_cluster -n 2 \"python $1 $sub\"
     done
 
 fi
